0ad1f7b7e8872609a4f5edc993349443f0bf13c2
munibafareed <fmuniba34@gmail.com>
2021-11-07 15:03:35 -0800

    work complete

---
+++ app.js
@@ -0,0 +1,96 @@
+// var siblings = ["zoya","tanzeel","bari","muniba"]
+// console.log(siblings)
+
+// var names =["ahmed","roshan"]
+// console.log(names)
+
+// var namesArr = ["aa","bb","cc","dd"];
+
+// console.log("ARRAY",namesArr)
+
+
+
+// chap#object for loop
+// q#3
+
+// for(var i = 1; i<=10 ; i++){
+//     console.log(i)
+
+// }
+// // q#4
+
+// // var name = prompt("enter a number to show its multiplication table")
+// // var name2 = prompt("enter length multiplication table")
+
+// // document.write("mutiplication table of " + "" + 2)
+// // document.write(" <br> length" + "" + 15 )
+
+// // for(var i = 1; i<=15 ; i++){
+// //     document.write("2 x"+ "" + i + "" + "=" + "" + 2*i + "<br>" )
+
+// }
+// q5#
+
+
+// var fruits =["apple","banana","mango","orange","strawbery"]
+// // console.log(fruits[0])
+// // console.log(fruits[1])
+// // console.log(fruits[2])
+// // console.log(fruits[3])
+// // console.log(fruits[4])
+
+// for(var i = 0; i < fruits.length ; i++){
+//     console.log (fruits[i])
+    
+// } 
+
+// q#7
+// var A = ["cake", "apple pie", "cookies", "chips","paties"]
+// var user = prompt("welcome to bakery..what do u want sir?")
+// for(var i = 0; i< A.length ; i++){
+//         if(user===A[i]){
+//         alert("bakery items is available")
+
+//     }
+
+    
+//     else{
+//         alert("we r sorry this items not available")
+//     }
+// }
+
+
+
+// q # 6 a
+
+
+// for(var i = 1 ; i <= 10 ; i++){
+//     console.log(i)
+
+// }
+
+
+
+
+// arrays quistion # 7
+
+// var qualification = ["1.ssc","2.hsc","3.bsc","4.bs","5.bcom","6.msc","7.phd"]
+// document.write(qualification[0] +"<br>")
+// document.write(qualification[1] +"<br>")
+// document.write(qualification[2] + "<br>")
+// document.write(qualification[3] + "<br>")
+// document.write(qualification[4] + "<br>")
+// document.write(qualification[5] + "<br>")
+// document.write(qualification[6] + "<br>")
+
+// q # 11
+// var cityArr = ["karachi","lahore","islamabad","quetta","peshawar"];
+// console.log("ARRAY" ,cityArr)
+// var copyArr = cityArr.slice(2 , 4 )
+// console.log("COPY ARR" , copyArr)
+
+
+
+
+
+
